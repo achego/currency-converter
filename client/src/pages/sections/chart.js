d94e89d9618a785147dcb8c2e883dfb5d00aee3a
Belema <tamunobelemaachego@gmail.com>
2023-01-16 18:00:37 +0100

    added base data fpr chart

--- client/src/pages/sections/chart.js
+++ client/src/pages/sections/chart.js
@@ -5,7 +5,12 @@ import {Chart, CategoryScale, LinearScale, LineElement, PointElement } from 'cha
 
 
 const Charts = () => {
-  const data = {}
+  // const data = {}
+  Chart.register(
+    LineElement,
+    CategoryScale,
+    PointElement,LinearScale
+  )
   return (
     <div className="px-9 flex flex-col w-full pt-12">
       <div className="flex justify-between">
@@ -18,9 +23,14 @@ const Charts = () => {
       {/* Chart */}
       <div className="w-full overflow-x-auto py-4">
         <Line data={{
-          labels: ['1', '2', '3'],
+          labels: ['1', '2', '3', '4', '5', '6'],
           datasets: [{
-            data: [45, 65, 22]
+            data: [18, 65, 22, 14, 21, 33],
+            borderColor: '#4FC9F8',
+            // backgroundColor: 'red',
+            tension: 0.5
+
+            
           }]
         }}></Line>
       </div>
